df61bd8c6fd1af004b127562292ac7ad86bfe9b5
Antonio Marcos Alves Morais <115155873+antonio-marcos1989@users.noreply.github.com>
2025-04-05 11:34:50 -0300

    Commit

---
+++ src/main/java/com/example/springboot_dataset/controller/ItemController.java
@@ -0,0 +1,31 @@
+package com.example.springboot_dataset.controller;
+
+import com.example.springboot_dataset.model.Item;
+import com.example.springboot_dataset.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping
+    public List<Item> getAllItems(){
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable String id){
+        return itemService.getItemById(id);
+    }
+
+}
---
+++ src/main/java/com/example/springboot_dataset/service/ItemService.java
@@ -0,0 +1,27 @@
+package com.example.springboot_dataset.service;
+
+import com.example.springboot_dataset.model.Item;
+import com.example.springboot_dataset.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems(){
+        return itemRepository.findAll();
+    }
+
+    public ResponseEntity<Item> getItemById(String id){
+        return itemRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
